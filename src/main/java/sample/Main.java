d7060dc0c7f5804f73f6281fecb4d4bce64782d7
Tomasz Więcek <tomasz.wiecek@ttms.pl>
2017-08-31 10:03:40 +0200

    Tray icon first version.

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,14 +1,21 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
+
 public class Main extends Application {
 
-    @Override
+    /*@Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
@@ -19,5 +26,97 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }*/
+
+    private boolean firstTime;
+    private TrayIcon trayIcon;
+
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        createTrayIcon(stage);
+        firstTime = true;
+        Platform.setImplicitExit(false);
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root, 300, 275));
+        stage.show();
+
+    }
+
+    public void createTrayIcon(final Stage stage) {
+        if (SystemTray.isSupported()) {
+            // get the SystemTray instance
+            SystemTray tray = SystemTray.getSystemTray();
+            // load an image
+            java.awt.Image image = null;
+            try {
+                URL url = new URL("http://i.imgur.com/o4LWcW1.png");
+                image = ImageIO.read(url);
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+
+
+            stage.setOnCloseRequest(t -> hide(stage));
+            // create a action listener to listen for default action executed on the tray icon
+            final ActionListener closeListener = e -> System.exit(0);
+
+            ActionListener showListener = e -> Platform.runLater(() -> stage.show());
+
+            ActionListener infoListener = e -> Platform.runLater(() -> System.out.println("costam"));
+            // create a popup menu
+            PopupMenu popup = new PopupMenu();
+
+            MenuItem showItem = new MenuItem("Show");
+            showItem.addActionListener(showListener);
+            popup.add(showItem);
+
+            MenuItem closeItem = new MenuItem("Close");
+            closeItem.addActionListener(closeListener);
+            popup.add(closeItem);
+
+            MenuItem infoItem = new MenuItem("Info");
+            infoItem.addActionListener(infoListener);
+            popup.add(infoItem);
+            /// ... add other items
+            // construct a TrayIcon
+            trayIcon = new TrayIcon(image, "Title", popup);
+            // set the TrayIcon properties
+            trayIcon.addActionListener(showListener);
+            // ...
+            // add the tray image
+            try {
+                tray.add(trayIcon);
+            } catch (AWTException e) {
+                System.err.println(e);
+            }
+            // ...
+        }
+    }
+
+    public void showProgramIsMinimizedMsg() {
+        if (firstTime) {
+            trayIcon.displayMessage("Some message.",
+                    "Some other message.",
+                    TrayIcon.MessageType.INFO);
+            firstTime = false;
+        }
+    }
+
+    private void hide(final Stage stage) {
+        Platform.runLater(() -> {
+			if (SystemTray.isSupported()) {
+				stage.hide();
+				showProgramIsMinimizedMsg();
+			} else {
+				System.exit(0);
+			}
+		});
     }
 }
